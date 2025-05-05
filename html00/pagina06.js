de6f7276dbb00fd35c83f305a95849245862b893
Aricas77 <carineftali@gmail.com>
2025-05-05 17:01:23 -0600

    06-eventos

---
+++ html00/pagina06.js
@@ -0,0 +1,11 @@
+const p = document.getElementById("app")
+p.innerHTML = Picture(1) + Picture(2) + Picture(3)
+
+const mensaje = (id) => {alert('Hola'+ id)}
+
+const Lista = AudioParamMap.querySelectorAll('img')
+console.log(Lista)
+for (let i = 0; i < Lista.length; i++) {
+    //const element = array[index];
+    
+} 
